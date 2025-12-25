86ef547ba93a7043b773601ec1f91bbad6bca890
Sean <seanivanfabia@gmail.com>
2025-12-25 14:32:47 +0800

    Restart: React frontend with Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        {/* Routes will be added here */}
+        <h1>The Pillar</h1>
+        <p>E-Publication Website - Coming Soon</p>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
+
